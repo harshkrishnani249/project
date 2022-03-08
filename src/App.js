cc44f18aecec7af659fdff5567541cc0364ab804
harshkrishnani249 <80941338+harshkrishnani249@users.noreply.github.com>
2022-03-08 10:26:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Herosection from './components/Herosection/Herosection';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/> 
+     <Herosection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Herosection/Herosection.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Pic from './pic1.jpg';
+import './Herosection.css';
+import Safe from './pic2.jpg';
+
+
+
+const Herosection = () => {
+    return (
+        <div className='container1'>
+
+
+          <h1 className='heading'>Better way 
+          
+            To invest</h1>
+         <p>Enjoy the feeling and experience of a  better way to invest in everything.
+              No, more struggling, striving, or even trying. It's just Better !
+         </p>
+
+            <img src={Pic} width="500" className='design' />
+
+        </div>
+
+       
+
+           
+
+
+
+
+
+
+     
+
+          
+            
+
+          
+        
+       
+       
+    )
+}
+
+export default Herosection
